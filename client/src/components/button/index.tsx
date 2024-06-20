945b16f1027c260dfc5ff35b1b6d0baab184688d
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-06-20 23:57:34 +0200

    add base layout for create page and modify page

--- client/src/components/button/index.tsx
+++ client/src/components/button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export default function Button({label, url}: ButtonProps) {
   return (
     <a href={url}>
-      <button className="bg-red-500 text-xs text-white px-10 py-3 rounded-3xl hover:bg-red-600">
+      <button className="bg-red-500 text-xs uppercase text-white px-10 py-3 rounded-3xl hover:bg-red-600">
         {label}
       </button>
     </a>
--- client/src/pages/recipe/create/[id].tsx
+++ client/src/pages/recipe/create/[id].tsx
@@ -1,12 +1,68 @@
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Button from '../../../components/Button'
+import '../../../app/globals.css';
 
 export default function UpdateRecipe() {
   const router = useRouter();
   const { id } = router.query;
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Update Recipe ID: {id}</h1>
-    </main>
+    <main className="min-h-screen">
+    <div className="w-full border py-10 px-4">
+      <Button label="List recipe" url={"/recipe"} />
+    </div>
+    <section className="space-y-5 w-2/3 p-24">
+      <h1 className="text-2xl font-semibold text-gray-800">Update Recipe</h1>
+      <form>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Name</label>
+          <input type="text" placeholder="insert name" className="border border-gray-200 shadow p-3 rounded-lg w-1/3" />
+        </div>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Ingredients</label>
+          <input type="text" placeholder="insert ingredients" className="border border-gray-200 shadow p-3 rounded-lg w-1/3" />
+        </div>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Instructions</label>
+          <input type="text" placeholder="insert instructions" className="border border-gray-200 shadow p-3 rounded-lg w-1/3" />
+        </div>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Cuisines</label>
+          <select className="border border-gray-200 shadow p-3 rounded-lg w-1/3">
+            <option value="0">...</option>
+            <option value="1">Italian</option>
+          </select>
+        </div>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Difficulties</label>
+          <select className="border border-gray-200 shadow p-3 rounded-lg w-1/3">
+            <option value="0">...</option>
+            <option value="1">Easy</option>
+            <option value="2">Medium</option>
+            <option value="3">Hard</option>
+          </select>
+        </div>
+        <div className="space-y-1 py-3">
+          <label className="text-lg font-semibold text-gray-800 block">Diets</label>
+          <select className="border border-gray-200 shadow p-3 rounded-lg w-1/3">
+            <option value="0">...</option>
+            <option value="1">Vegetarian</option>
+            <option value="2">Mediterranean</option>
+            <option value="3">Non-Vegetarian</option>
+            <option value="4">Pescatarian</option>
+          </select>
+        </div>
+        <div className="flex items-center py-3 space-x-4">
+          <button className="bg-red-500 text-xs uppercase text-white px-10 py-3 rounded-3xl hover:bg-red-600">
+            salva
+          </button>
+          <button className="bg-gray-500 text-xs uppercase text-white px-10 py-3 rounded-3xl hover:bg-gray-600">
+            reset
+          </button>
+        </div>
+      </form>
+    </section>
+  </main>
   );
 };
