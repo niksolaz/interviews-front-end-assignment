0706bb746e395b7b54484f1503e6269be3c0ab76
Nicola Solazzo <solazzo.nicola@gmail.com>
2024-06-20 18:44:45 +0200

    create recipe ingredient and other image

--- client/src/pages/recipe/[id].tsx
+++ client/src/pages/recipe/[id].tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import '../../app/globals.css';
 
 export default function Recipe() {
   const [recipe, setRecipe] = useState([]);
@@ -26,13 +27,28 @@ export default function Recipe() {
   }, [id]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Recipe Details: {id}</h1>
+    <main className="p-24">
       <section>
       {error ? (
             <p>Error fetching recipes: {error.message}</p>
           ) : (
-            <p>{recipe.name}</p>
+            <section className="space-y-5">
+              <h1 className="text-2xl font-semibold text-gray-800">Recipe name {recipe.name}</h1>
+              <div className="bg-gray-200">
+                <img src={path + recipe.image} className="object-cover h-[500px] w-full rounded-lg" />
+              </div>
+              <div>
+                <h1 className="text-2xl font-semibold text-gray-800">Recipe ingredients:</h1>
+                <ul>
+                  {recipe.ingredients && recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h1 className="text-2xl font-semibold text-gray-800">Reviews:</h1>
+              </div>
+            </section>
           )}
       </section>
     </main>
